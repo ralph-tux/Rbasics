e5fbd6cef09f0de5ce2d6a3a08efdb36c80a2c0a
Ralph <59715335+ralph-tux@users.noreply.github.com>
2020-07-31 20:30:01 -0300

    Create logical_operators_AND_coercion.R

---
+++ logical_operators_AND_coercion.R
@@ -0,0 +1,19 @@
+###Logical operators###############
+
+== Equal to
+!= Not equal to
+< Less than
+> Greater than
+<= Less than or equal to
+>= Greater than or equal to
+is.na(x) Missing?
+& Logical AND
+| Logical OR
+! Logical NOT
+
+###Coercion
+
+as.numeric(x) Convert to numeric
+as.character(x) Convert to text string
+as.logical(x) Convert to logical
+factor(x) Create factor from vector x
